5823e48917b0a97f007eee338b7947196934c59d
DariaSeliverstova <dashulia2004@yandex.ru>
2022-06-20 14:26:53 -0400

    added files

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class CashbackHackServiceTest {
+    @Test
+    void showWorkCashback(){
+        int boundary = 2000;
+        int amount = 10;
+        int expected = 1000;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(boundary);
+
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    void showWorkCashback2(){
+        int boundary = 900;
+        int amount = 10;
+        int expected = 100;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(boundary);
+
+        assertEquals(expected,actual);
+
+
+    }
+
+
+    @Test
+    void showWorkCashback3(){
+        int boundary = 1000;
+        int amount = 10;
+        int expected = 100;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(boundary);
+
+        assertEquals(expected,actual);
+
+
+    }
+
+
+}
